5ee1e685c90eec4c1209c175f00a64e4634b8761
zander <676993761@qq.com>
2020-11-15 21:18:50 +0800

    log

--- components/logger/zlog/log.go
+++ components/logger/zlog/log.go
@@ -7,54 +7,81 @@ import (
 )
 
 func (this *ZLog) Debug(msg string, field ...logger.Field) {
-	this.engine.Debug(msg, this.fs(field...)...)
+	if len(field) > 0 {
+		for _, v := range field {
+			this.engine.Debug(msg, this.fs(v)...)
+		}
+	} else {
+		this.engine.Debug(msg)
+	}
+
 }
 
 func (this *ZLog) Info(msg string, field ...logger.Field) {
-	this.engine.Info(msg, this.fs(field...)...)
+	if len(field) > 0 {
+		for _, v := range field {
+			this.engine.Info(msg, this.fs(v)...)
+		}
+	} else {
+		this.engine.Info(msg)
+	}
 }
 
 func (this *ZLog) Error(msg string, field ...logger.Field) {
-	this.engine.Error(msg, this.fs(field...)...)
+	if len(field) > 0 {
+		for _, v := range field {
+			this.engine.Error(msg, this.fs(v)...)
+		}
+	} else {
+		this.engine.Error(msg)
+	}
 }
 
 func (this *ZLog) Panic(msg string, field ...logger.Field) {
-	this.engine.Panic(msg, this.fs(field...)...)
+	if len(field) > 0 {
+		for _, v := range field {
+			this.engine.Panic(msg, this.fs(v)...)
+		}
+	} else {
+		this.engine.Panic(msg)
+	}
 }
 
 func (this *ZLog) Fatal(msg string, field ...logger.Field) {
-	this.engine.Fatal(msg, this.fs(field...)...)
+	if len(field) > 0 {
+		for _, v := range field {
+			this.engine.Fatal(msg, this.fs(v)...)
+		}
+	} else {
+		this.engine.Fatal(msg)
+	}
 }
 
-func (this *ZLog) fs(data ...logger.Field) (fields []zap.Field) {
+func (this *ZLog) fs(data logger.Field) (fields []zap.Field) {
+	fields = make([]zap.Field, 0)
+	typ := reflect.TypeOf(data)
+	val := reflect.ValueOf(data)
+	if val.Kind() != reflect.Struct {
+		return fields
+	}
+	for i := 0; i < val.NumField(); i++ {
+		if typ.Field(i).Tag.Get("json") == "ts" || typ.Field(i).Tag.Get("json") == "level" || typ.Field(i).Tag.Get("json") == "msg" {
+			continue
+		}
 
-	if len(data) > 0 {
-		fields = make([]zap.Field, 0)
-		for _, v := range data {
-			typ := reflect.TypeOf(v)
-			val := reflect.ValueOf(v)
-			if val.Kind() != reflect.Struct {
-				return fields
-			}
-			for i := 0; i < val.NumField(); i++ {
-				if typ.Field(i).Tag.Get("json") == "ts" || typ.Field(i).Tag.Get("json") == "level" || typ.Field(i).Tag.Get("json") == "msg" {
-					continue
-				}
-
-				if typ.Field(i).Tag.Get("json") != "" {
-					if val.Field(i).Kind() == reflect.Map {
-						for _, mk := range val.Field(i).MapKeys() {
-							if mk.String() == "ts" || mk.String() == "level" || mk.String() == "msg" {
-								continue
-							}
-							fields = append(fields, zap.Any(mk.String(), val.Field(i).MapIndex(mk).Elem().Interface()))
-						}
-					} else {
-						fields = append(fields, zap.Any(typ.Field(i).Tag.Get("json"), val.Field(i).Interface()))
+		if typ.Field(i).Tag.Get("json") != "" {
+			if val.Field(i).Kind() == reflect.Map {
+				for _, mk := range val.Field(i).MapKeys() {
+					if mk.String() == "ts" || mk.String() == "level" || mk.String() == "msg" {
+						continue
 					}
+					fields = append(fields, zap.Any(mk.String(), val.Field(i).MapIndex(mk).Elem().Interface()))
 				}
+			} else {
+				fields = append(fields, zap.Any(typ.Field(i).Tag.Get("json"), val.Field(i).Interface()))
 			}
 		}
 	}
+
 	return fields
 }
