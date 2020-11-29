f50efd9e56b9a64415407cf23254d7451c8e638b
zander <676993761@qq.com>
2020-11-29 19:13:29 +0800

    init

--- components/helper/pipeline/pipeline_test.go
+++ components/helper/pipeline/pipeline_test.go
@@ -11,30 +11,43 @@ func TestPipeline_DOPipeline(t *testing.T) {
 	a := NewPipeline()
 	done := make(chan struct{}, 1)
 	defer close(done)
-	c1 := Event{
-		Ctx: context.Background(),
-		Handler: func(e *Event) error {
-			data := e.Input.(int)
-			e.Output = data * 9
-			return nil
-		},
-		Input: 11,
-		Error: nil,
+	//c1 := Event{
+	//	Ctx: context.Background(),
+	//	Handler: func(e *Event) error {
+	//		data := e.Input.(int)
+	//		e.Output = data * 9
+	//		return nil
+	//	},
+	//	Input: 11,
+	//	Error: nil,
+	//}
+	//ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//defer cancel()
+	//c2 := Event{
+	//	Ctx: ctx,
+	//	Handler: func(e *Event) error {
+	//		time.Sleep(2 * time.Second)
+	//		data := e.Input.(int)
+	//		e.Output = data * 9
+	//		return nil
+	//	},
+	//	Input: 22,
+	//	Error: nil,
+	//}
+	events := make([]Event, 0)
+	for i := 0; i <= 100; i++ {
+		events = append(events, Event{
+			Ctx: context.Background(),
+			Handler: func(e *Event) error {
+				time.Sleep(3 * time.Second)
+				data := e.Input.(int)
+				e.Output = data * 9
+				return nil
+			},
+			Input: 22,
+			Error: nil,
+		})
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-	c2 := Event{
-		Ctx: ctx,
-		Handler: func(e *Event) error {
-			time.Sleep(2 * time.Second)
-			data := e.Input.(int)
-			e.Output = data * 9
-			return nil
-		},
-		Input: 22,
-		Error: nil,
-	}
-
-	t.Log(a.Do(done, c1, c2))
-	time.Sleep(10 * time.Second)
+	t.Log(a.Do(done, events...))
+	//time.Sleep(10 * time.Second)
 }
