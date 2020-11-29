464f0938008eba10bbba1b99a94e1b1738240893
zander <676993761@qq.com>
2020-11-29 19:18:32 +0800

    init

--- components/helper/unique.go
+++ components/helper/unique.go
@@ -2,13 +2,24 @@ package helper
 
 import (
 	"fmt"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
 var _id int64
+var _start string
+var _uniqueLock sync.Mutex
 
+func UniqueIDStart() string {
+	return time.Now().Format("60102150405")
+}
 func UniqueID(prefix string) string {
+	if _start == "" {
+		_uniqueLock.Lock()
+		_start = UniqueIDStart()
+		_uniqueLock.Unlock()
+	}
 	atomic.AddInt64(&_id, 1)
-	return fmt.Sprintf("%s%d%s", prefix, _id, time.Now().Format("60102150405"))
+	return fmt.Sprintf("%s%d%s", prefix, _id, _start)
 }
