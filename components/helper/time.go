e191818293759e13b05d49b7ecaed71b16b66b01
张平龙 <bjzhangpinglong@yeah.net>
2021-08-05 10:21:01 +0800

    时间转换

--- components/helper/time.go
+++ components/helper/time.go
@@ -58,9 +58,57 @@ func (t *Time) DayTime(calcDay int) time.Time {
 	return today
 }
 
-func (t *Time) UnixTime(unix int64) time.Time {
-	return time.Unix(unix, 0).In(t.location)
+//UnixToTime  时间戳转时间 sec秒和 nsec纳秒
+func (t *Time) UnixToTime(sec int64, nsec int64) time.Time {
+	return time.Unix(sec, nsec).In(t.location)
 }
+
+//Unix 时间戳秒 例：1628127247
+func (t *Time) Unix() int64 {
+	return t.Now().Unix()
+}
+
+//UnixNano 时间戳纳秒 例：1628127212214105000
+func (t *Time) UnixNano() int64 {
+	return t.Now().UnixNano()
+}
+
+//UnixMsec 时间戳毫秒级 例：1628127443176
+func (t *Time) UnixMsec() int64 {
+	return t.Now().UnixNano() / 1e6
+}
+
+//TimeToUnixNano 时间转时间戳纳秒级
+//例：timeIns.TimeToUnixNano("2006-01-02T15:04:05.999999999", "2006-01-02T15:04:05.999999999")
+//返回结果 1136185445999999999
+func (t *Time) TimeToUnixNano(source string, layout string) (int64, error) {
+	sourceTime, err := time.ParseInLocation(layout, source, t.location)
+	if err != nil {
+		return 0, err
+	}
+	return sourceTime.UnixNano(), err
+}
+
+//TimeToUnix 时间转时间戳纳秒级
+//例：TimeToUnix("2010-03-04", "2006-01-02") = 1265126400
+func (t *Time) TimeToUnix(source string, layout string) (int64, error) {
+	unixNano, err := t.TimeToUnixNano(source, layout)
+	if err != nil {
+		return 0, err
+	}
+	return unixNano / 1e9, nil
+}
+
+//TimeToUnixMsec 时间转时间戳毫秒级
+//例：TimeToUnixMsec("2010-03-04", "2006-01-02") = 1265126400000
+func (t *Time) TimeToUnixMsec(source string, layout string) (int64, error) {
+	unixNano, err := t.TimeToUnixNano(source, layout)
+	if err != nil {
+		return 0, err
+	}
+	return unixNano / 1e6, nil
+}
+
 func (t *Time) Year() string {
 	return t.Now().Format("2006")
 }
