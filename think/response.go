82c9b8dd57270e206d907e1b28fa027aef9848d4
zander <676993761@qq.com>
2021-08-06 11:03:52 +0800

    resp

--- think/response.go
+++ think/response.go
@@ -5,7 +5,7 @@ import "fmt"
 var ResponseDebug = false
 
 type ResponseError struct {
-	Code    Code
+	Err     Code
 	SubCode int `json:",omitempty"`
 	Msg     string
 	Data    interface{}
@@ -14,13 +14,13 @@ type ResponseError struct {
 func NewResponseFromErr(err error, debug bool) *ResponseError {
 
 	var r = ResponseError{
-		Code:    Err2Code(err),
+		Err:     Err2Code(err),
 		SubCode: 0,
 		Msg:     Err2Code(err).ToString(),
 		Data:    nil,
 	}
 
-	if r.Code != CodeSystemSpaceError {
+	if r.Err != CodeSystemSpaceError {
 		r.Data = err.Error()
 	}
 	if IsErrBiz(err) {
@@ -36,7 +36,7 @@ func NewResponseBytesFromErr(err error, debug bool) []byte {
 	return []byte(NewResponseStringFromErr(err, debug))
 }
 
-var errNilString = fmt.Sprintf(`{"Code":%d,"Msg":"%s","Data":"%s"}`, CodeException, CodeException.ToString(), "")
+var errNilString = fmt.Sprintf(`{"Error":%d,"Msg":"%s","Data":"%s"}`, CodeException, CodeException.ToString(), "")
 
 func NewResponseStringFromErr(err error, debug bool) string {
 	if err == nil {
@@ -53,8 +53,8 @@ func NewResponseStringFromErr(err error, debug bool) string {
 	}
 
 	if IsErrBiz(err) {
-		return fmt.Sprintf(`{"Code":%d,"Msg":"%s","SubCode":%d,"Data":"%s"}`, code, code.ToString(), BizCode(err), data)
+		return fmt.Sprintf(`{"Error":%d,"Msg":"%s","SubCode":%d,"Data":"%s"}`, code, code.ToString(), BizCode(err), data)
 	} else {
-		return fmt.Sprintf(`{"Code":%d,"Msg":"%s","Data":"%s"}`, code, code.ToString(), data)
+		return fmt.Sprintf(`{"Error":%d,"Msg":"%s","Data":"%s"}`, code, code.ToString(), data)
 	}
 }
