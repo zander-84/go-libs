f26effbe7185d89a67736fb42703054b8fae4eaf
zander <676993761@qq.com>
2021-09-01 15:55:31 +0800

    code

--- think/response.go
+++ think/response.go
@@ -1,48 +1,27 @@
 package think
 
-import "fmt"
+import (
+	"encoding/json"
+)
 
 var ResponseDebug = false
 
-var ResponseCode = "Code"
-var ResponseKeyMsg = "Msg"
-var ResponseKeyData = "Data"
-var ResponseKeySubCode = "SubCode"
-
-func ResponseSuccessData(data interface{}) map[string]interface{} {
-	return map[string]interface{}{
-		ResponseKeyData: data,
-	}
-}
-
-func ResponseSuccessSource(data interface{}) interface{} {
-	return data
-}
-
-func ResponseSuccessDataWithSubCode(subCode int32, data interface{}) map[string]interface{} {
-	return map[string]interface{}{
-		ResponseKeySubCode: subCode,
-		ResponseKeyData:    data,
-	}
-}
-
-type ResponseError struct {
-	Err     Code
+type Response struct {
+	Code    Code
 	SubCode int `json:",omitempty"`
 	Msg     string
 	Data    interface{}
 }
 
-func NewResponseFromErr(err error, debug bool) *ResponseError {
-
-	var r = ResponseError{
-		Err:     Err2Code(err),
+func NewResponseFromErr(err error, debug bool) *Response {
+	var r = Response{
+		Code:    Err2Code(err),
 		SubCode: 0,
 		Msg:     Err2Code(err).ToString(),
 		Data:    nil,
 	}
 
-	if r.Err != CodeSystemSpaceError {
+	if r.Code != CodeSystemSpaceError {
 		r.Data = err.Error()
 	}
 	if IsErrBiz(err) {
@@ -55,28 +34,6 @@ func NewResponseFromErr(err error, debug bool) *ResponseError {
 }
 
 func NewResponseBytesFromErr(err error, debug bool) []byte {
-	return []byte(NewResponseStringFromErr(err, debug))
-}
-
-var errNilString = fmt.Sprintf(`{"%s":%d,"%s":"%s","%s":"%s"}`, ResponseCode, CodeException, ResponseKeyMsg, CodeException.ToString(), ResponseKeyData, "")
-
-func NewResponseStringFromErr(err error, debug bool) string {
-	if err == nil {
-		return errNilString
-	}
-
-	var data string
-	var code = Err2Code(err)
-	if code != CodeSystemSpaceError {
-		data = err.Error()
-	}
-	if debug && data != "" {
-		data = err.Error()
-	}
-
-	if IsErrBiz(err) {
-		return fmt.Sprintf(`{"%s":%d,"%s":"%s","%s":%d,"%s":"%s"}`, ResponseCode, code, ResponseKeyMsg, code.ToString(), ResponseKeySubCode, BizCode(err), ResponseKeyData, data)
-	} else {
-		return fmt.Sprintf(`{"%s":%d,"%s":"%s","%s":"%s"}`, ResponseCode, code, ResponseKeyMsg, code.ToString(), ResponseKeyData, data)
-	}
+	res, _ := json.Marshal(NewResponseFromErr(err, debug))
+	return res
 }
