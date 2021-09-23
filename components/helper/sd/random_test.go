99b33aaa2b8548d7e0379371688de6e2c5d5587d
zander <676993761@qq.com>
2021-09-23 15:29:09 +0800

    test

--- components/helper/sd/random_test.go
+++ components/helper/sd/random_test.go
@@ -1,8 +1,9 @@
 package sd
 
 import (
+	"errors"
+	"sync"
 	"testing"
-	"time"
 )
 
 func TestRandom(t *testing.T) {
@@ -13,9 +14,12 @@ func TestRandom(t *testing.T) {
 		"127.0.0.1:8083": 1,
 	})
 	rr := NewRandom(l, true)
-
-	for i := 0; i < 1000000; i++ {
+	w := sync.WaitGroup{}
+	for i := 0; i < 10000000; i++ {
+		w.Add(1)
 		go func(i int) {
+			defer w.Done()
+
 			if d, err := rr.Next(); err != nil {
 				t.Error(err.Error())
 			} else if d == "" {
@@ -23,10 +27,24 @@ func TestRandom(t *testing.T) {
 			}
 		}(i)
 	}
-	go func() {
-		l.Remove("127.0.0.1:8082")
-	}()
-	time.Sleep(3 * time.Second)
+	for i := 0; i < 100000; i++ {
+		w.Add(1)
+		go func(i int) {
+			defer w.Done()
+			l.Set(map[string]int{
+				"127.0.0.1:8081": 1,
+				"127.0.0.1:8084": 1,
+				"127.0.0.1:8085": 1,
+			})
+			if i == 1000 {
+				l.RecordErr(errors.New("some wrong"))
+			} else {
+				l.CleanErr()
+			}
+		}(i)
+	}
+
+	w.Wait()
 	t.Log(rr.Used())
 }
 
