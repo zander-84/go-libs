dcd608acdf0b88d5c52b4fa479c3c74c517cfae6
zander <676993761@qq.com>
2021-10-20 19:51:10 +0800

    tool

--- components/helper/tools.go
+++ components/helper/tools.go
@@ -4,7 +4,45 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"strings"
 )
+
+var tool = NewTool()
+
+type Tool struct{}
+
+func NewTool() *Tool { return new(Tool) }
+
+func GetTool() *Tool { return tool }
+
+func (t *Tool) InStringArr(s string, ss []string) bool {
+	for _, v := range ss {
+		if s == v {
+			return true
+		}
+	}
+	return false
+}
+func (t *Tool) InIntArr(s int, ss []int) bool {
+	for _, v := range ss {
+		if s == v {
+			return true
+		}
+	}
+	return false
+}
+
+func (t *Tool) GetLastIndexString(s string, substr string) string {
+	return s[strings.LastIndex(s, substr)+len(substr):]
+}
+
+func (t *Tool) GetLastIndexInt(s string, substr string) int {
+	return defaultConv.ShouldStoI(s[strings.LastIndex(s, substr)+len(substr):])
+}
+
+func (t *Tool) GetLastIndexInt64(s string, substr string) int64 {
+	return defaultConv.ShouldStoI64(s[strings.LastIndex(s, substr)+len(substr):])
+}
 
 func PrettyPrint(v interface{}) {
 	b, err := json.Marshal(v)
