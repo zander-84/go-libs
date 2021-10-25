6ac43672c00efa633588975d8992273ae3054fcd
zander <676993761@qq.com>
2021-10-25 16:36:44 +0800

    sync

--- components/helper/sync/go.go
+++ components/helper/sync/go.go
@@ -1,14 +1,34 @@
 package sync
 
+import (
+	"context"
+	"time"
+)
+
 func Go(fn func()) {
 	go func() {
 		defer func() {
-			defer func() {
-				if err := recover(); err != nil {
-					return
-				}
-			}()
+			if err := recover(); err != nil {
+				return
+			}
 		}()
 		fn()
 	}()
+}
+
+func RunWithTimeout(fn func() error, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer func() {
+		cancel()
+	}()
+	fin := make(chan error, 1)
+	go func() {
+		fin <- fn()
+	}()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case e := <-fin:
+		return e
+	}
 }
